afdfe479ed76c186b068307e97d66253175db0b3
Angelika Brown <angelikpbrown@gmail.com>
2025-03-17 08:47:14 -0400

    Passing tests

--- temperature_utils.py
+++ temperature_utils.py
@@ -8,7 +8,11 @@ def convert_to_celsius(fahrenheit_temp: float) -> float:
     :param fahrenheit_temp: A float representing a temperature in fahrenheit
     :return: A float representing the corresponding value of the fahrenheit_temp parameter in celsius
     """
-    return (fahrenheit_temp - 32) / 1.8
+    if fahrenheit_temp > 0:
+       temp = (fahrenheit_temp - 32) / (9/5)
+    elif fahrenheit_temp < 0:
+        temp = (fahrenheit_temp - 32) * (5/9)
+    return round(temp, 2)
 
 
 
@@ -19,7 +23,8 @@ def convert_to_fahrenheit(celsius_temp: float) -> int:
     :param celsius_temp: A float representing a temperature in celsius
     :return:  A float representing the corresponding value of the celsius_temp parameter in fahrenheit
     """
-    pass  # remove pass statement and implement me
+    temp = (celsius_temp / 9/5) + 32
+    return int(temp)
 
 
 def temperature_tuple(temperatures: Iterable, input_unit_of_measurement: str) -> Tuple[Tuple[float, float]]:
